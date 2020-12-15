5037171c5b252a061739f4c96b2fe1a5011fe070
Landcruiser87 <andyheroy@gmail.com>
2020-12-15 15:32:46 -0500

    cleaning up

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,22 +1,31 @@
-class Garden:
-'''
-Define the plant dictionary and student list for reference to placement chart.
-'''
-	plant_dic = {
+class Garden():
+# '''
+# Define the plant dictionary and student list for reference to placement chart.
+
+# First splits the diagram into 2 rows
+# Sorts the students alphabetically (As that is how they're assigned)
+# '''
+plant_dic = {
 		"C": "Clover",
 		"G": "Grass",
 		"R": "Radishes",
 		"V": "Violets"}
-	students = ['Alice','Bob','Charlie','David',
+
+students = ['Alice','Bob','Charlie','David',
 				'Eve','Fred','Ginny','Harriet',
 				'Ileana','Joseph','Kincaid','Larry']
 
-    def __init__(self, diagram, students=students):
-        self.diagram = diagram.splitlines("/n")
+	def __init__(self, diagram, students=students):
+		self.diagram = diagram.splitlines("/n")
 		self.students = sorted(students)
+		self.plants(diagram, students)
 
-	def plants(self,students):
-		student_names = [x[0] for x in self.students]
+	def plants(self,diagram, students):
+		for x, student in enumerate(students)
+			start = 2 * x
+			end = start + 2
+			plants = ''.join(row[start:end] for row in diagram)
+			self.student = [self.plant_dic[p] for p in plants]
 		
 		
 
@@ -27,37 +36,26 @@ import unittest
 
 
 class KindergartenGardenTest(unittest.TestCase):
-    def test_partial_garden_garden_with_single_student(self):
-        garden = Garden("RC\nGG")
-        self.assertEqual(
-            garden.plants("Alice"), ["Radishes", "Clover", "Grass", "Grass"]
-        )
+	def test_partial_garden_garden_with_single_student(self):
+		garden = Garden("RC\nGG")
+		self.assertEqual(
+			garden.plants("Alice"), ["Radishes", "Clover", "Grass", "Grass"]
+		)
 
-    def test_partial_garden_different_garden_with_single_student(self):
-        garden = Garden("VC\nRC")
-        self.assertEqual(
-            garden.plants("Alice"), ["Violets", "Clover", "Radishes", "Clover"]
-        )
+	def test_partial_garden_different_garden_with_single_student(self):
+		garden = Garden("VC\nRC")
+		self.assertEqual(
+			garden.plants("Alice"), ["Violets", "Clover", "Radishes", "Clover"]
+		)
 
-    def test_partial_garden_garden_with_two_students(self):
-        garden = Garden("VVCG\nVVRC")
-        self.assertEqual(
-            garden.plants("Bob"), ["Clover", "Grass", "Radishes", "Clover"]
-        )
+	def test_partial_garden_garden_with_two_students(self):
+		garden = Garden("VVCG\nVVRC")
+		self.assertEqual(
+			garden.plants("Bob"), ["Clover", "Grass", "Radishes", "Clover"]
+		)
 
 if __name__ == "__main__":
-    unittest.main()
+	unittest.main()
 
 
 
-# "C:\Program Files (x86)\Microsoft Office\root\Office16\MSACCESS.EXE" "C:\Users\andyh\Google Drive\Job\UpWork\Cleveland\OldVersion\Cleveland Golf R&D Database.mdb" /WRKGRP "C:\Users\andyh\Google Drive\Job\UpWork\Cleveland\OldVersion\Secured.mdw"
-
-
-
-#   right:
-#     - icon: fa-envelope fa-lg
-#       href: contact.html
-#     - icon: fa-github fa-lg
-#       href: https://github.com/Landcruiser87
-#     - icon: <i class="fab fa-linkedin-in"></i>
-#       href: https://www.linkedin.com/in/data-science-and/y
