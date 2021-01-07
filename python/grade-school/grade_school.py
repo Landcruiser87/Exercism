5bc7c221f2f83e671f5e08daf9026bc9bfad513d
Landcruiser87 <andyheroy@gmail.com>
2021-01-07 10:29:06 -0500

    Completed grade_school.

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,12 +1,43 @@
 class School:
-    def __init__(self):
-        self.db = []
+	'''
+	The main utility of this class is to add students names and grades to create a dictionary roster
+	'''
+	def __init__(self):
+		'''
+		Initializes empty dict
+		Args:
+		student = empty dict
+		'''
+		self.student = {}
 		
-    def add_student(self, name, grade):
-        self.db.append()
+	def add_student(self, name, grade):
+		'''
+		Adds the name and grade to the dictionary
+		Args:
+			name = student name
+			grade = grade
+		Returns:
+			dictionary of student and grade
+		'''
+		self.student[name] = grade
 
-    def roster(self):
-        pass
+	def roster(self):
+		'''
+		Generates a roster for the class from the dictionary above
+		Args:
+			ros_list = List of students sorted by their name and grade
+		Returns: 
+			list of sorted student names
+		'''
+		ros_list = sorted(self.student.items(), key=lambda x:(x[1], x[0]))
+		return [item[0] for item in ros_list]
 
-    def grade(self, grade_number):
-        pass
+	def grade(self, grade_number):
+		'''
+		Generates a list of all students in a grade. 
+		Args:
+			grade_number = The grade of students to return
+		Returns:
+			list of sorted students in that particular grade.
+		'''
+		return sorted([name for name, grade in self.student.items() if grade == grade_number])
