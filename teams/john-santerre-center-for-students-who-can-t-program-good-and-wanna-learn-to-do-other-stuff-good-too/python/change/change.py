e93b0d22634f149b5c18e770b0765aa7f9bb070b
Landcruiser87 <andyheroy@gmail.com>
2021-05-17 10:41:49 -0400

    food chain on person and starting change

--- teams/john-santerre-center-for-students-who-can-t-program-good-and-wanna-learn-to-do-other-stuff-good-too/python/change/change.py
+++ teams/john-santerre-center-for-students-who-can-t-program-good-and-wanna-learn-to-do-other-stuff-good-too/python/change/change.py
@@ -1,4 +1,10 @@
+from itertools import permutations
+from collections import Counter
+
 def find_fewest_coins(coins, target):
-    if target < 0:
-		raise ValueError('Need a positive change value')
+	#Get all combinations of coin
+	coinCombo = [x for x in permutations(coins,2)]
 	
+	print(colorCount)
+
+test = find_fewest_coins([1, 5, 10, 25, 100], 15)
