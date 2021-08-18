be247ddda366f0c801485d1b0c3329c902fc5579
Landcruiser87 <andyheroy@gmail.com>
2021-08-18 15:14:36 -0400

    Finished Binary Search

--- teams/john-santerre-center-for-students-who-can-t-program-good-and-wanna-learn-to-do-other-stuff-good-too/python/binary-search/binary_search.py
+++ teams/john-santerre-center-for-students-who-can-t-program-good-and-wanna-learn-to-do-other-stuff-good-too/python/binary-search/binary_search.py
@@ -1,2 +1,34 @@
 def find(search_list, value):
-    pass
+	if len(search_list) == 0:
+		raise ValueError("No empty lists allowed!!")
+	
+	mid_idx = len(search_list) // 2
+
+	if value == search_list[mid_idx]:
+		return mid_idx
+	elif value < search_list[mid_idx]:
+		return find(search_list[:mid_idx], value)
+	elif value > search_list[mid_idx]:
+		return find(search_list[mid_idx+1:], value) + mid_idx + 1
+
+
+# def find(search_list, value):
+# 	if len(search_list) == 0:
+# 		raise ValueError("No empty lists allowed!!")
+	
+# 	left, right = 0, len(search_list) - 1
+# 	while left <= right:
+# 		mid_idx = (left + right) // 2
+# 		if value == search_list[mid_idx]:
+# 			return mid_idx
+# 		elif value < search_list[mid_idx]:
+# 			right = right - 1
+# 		elif value > search_list[mid_idx]:
+# 			left = left + 1
+
+# 	raise ValueError("Item not in list")
+
+search_list = [1, 3, 4, 6, 8, 9, 11]
+value = 5
+
+print(find(search_list, value))
