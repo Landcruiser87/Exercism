ce2be8b9ddd35eaab07b4bc3a5701fb6bae8f1b6
Landcruiser87 <andyheroy@gmail.com>
2021-11-23 09:05:41 -0500

    finished series

---
+++ python/series/series.py
@@ -0,0 +1,12 @@
+def slices(series, length):
+	
+	if length == 0:
+		raise ValueError("slice length cannot be zero")
+	if length < 0:
+		raise ValueError("slice length cannot be negative")
+	if len(series) == 0:
+		raise ValueError("series cannot be empty")
+	if len(series) < length:
+		raise ValueError("slice length cannot be greater than series length")
+	
+	return [series[x:x+length] for x in range(0, len(series) - length + 1)]
