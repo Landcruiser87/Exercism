374f0264f5967aa00c979d4ea9b4d716c9438a94
Landcruiser87 <andyheroy@gmail.com>
2021-11-26 01:00:25 -0500

    finished isbn_verifier

---
+++ python/isbn-verifier/isbn_verifier.py
@@ -0,0 +1,14 @@
+#
+
+def is_valid(isbn:str)->bool:
+	isbn = isbn.replace('-', '')
+	if len(isbn) != 10:
+		return False
+	isbn = list(isbn)
+
+	if isbn[-1] == "X":
+		isbn[-1] = "10"
+	for x in isbn:
+		if x.isdigit() == False:
+			return False
+	return sum(int(i) * (10 - j) for j, i in enumerate(list(isbn))) % 11 == 0
