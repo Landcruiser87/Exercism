426e669dee182af690e2629cdffe7264359f76ed
Landcruiser87 <andyheroy@gmail.com>
2021-12-01 09:00:13 -0600

    finished prime factors

---
+++ python/prime-factors/prime_factors.py
@@ -0,0 +1,20 @@
+def factors(value: int) -> list:
+	"""[Prime Factor Finder]
+
+	Args:
+		value ([int]): [Input value to find prime factors]
+
+	Returns:
+		[list]: [List of possible prime factors]
+	"""
+	factors = []
+	i = 2
+
+	while i - 1 < value:
+		while value % i == 0:
+			factors.append(i)
+			value = value / i
+		i += 1			
+	return factors
+
+print(factors(27))
