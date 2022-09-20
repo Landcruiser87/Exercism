ec148e6f4a1f21912637273d737158cd4d9093e3
Landcruiser87 <andyheroy@gmail.com>
2022-09-20 12:55:54 -0500

    Finished Simple Cipher

--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -1,9 +1,31 @@
+import secrets
+from string import ascii_lowercase
+from itertools import cycle
 class Cipher:
-    def __init__(self, key=None):
-        pass
+	def __init__(self, key=None):
+		if key is None:
+			self.key = self._random_key()
+		else:
+			self.key = key
+			if not self.key.isalpha():
+				raise ValueError("Key must be only letters")
+			
+			self.key = self.key.lower()
+		
+		self.LETTER_DICT = {x: idx for idx, x in enumerate(ascii_lowercase)}
 
-    def encode(self, text):
-        pass
+	def _random_key(self):
+		return ''.join(secrets.choice(ascii_lowercase) for _ in range(100))
 
-    def decode(self, text):
-        pass
+	def encode(self, text):
+		encoded_text = ""
+		for key_let, letter in zip(cycle(self.key), text):
+			encoded_text += ascii_lowercase[(self.LETTER_DICT[letter] + self.LETTER_DICT[key_let]) % 26]
+		return encoded_text
+	
+	def decode(self, text):
+		decoded_text = ""
+		for key_let, letter in zip(cycle(self.key), text):
+			decoded_text += ascii_lowercase[(self.LETTER_DICT[letter] - self.LETTER_DICT[key_let]) % 26]
+		return decoded_text
+
